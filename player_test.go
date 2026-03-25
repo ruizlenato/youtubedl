99ecd1050f729422bb1863ee8cfd67354faf2025
Luiz Renato <ruizlenato@proton.me>
2026-03-25 19:15:28 -0300

    test(player): align TestPlayer with current extraction behavior

--- player_test.go
+++ player_test.go
@@ -11,26 +11,14 @@ import (
 func TestPlayer(t *testing.T) {
 	p, err := NewPlayer()
 	if err != nil {
-		t.Errorf("failed to create player: %v", err)
+		t.Fatalf("failed to create player: %v", err)
 	}
 
-	if p.sig_sc == "" {
-		t.Errorf("p.sig_sc is empty")
-	}
-	if p.sig_timestamp == 0 {
-		t.Errorf("p.sig_timestamp is zero")
-	}
-	if p.nsig_name == "" {
-		t.Errorf("p.nsig_name is empty")
-	}
-	if p.nsig_sc == "" {
-		t.Errorf("p.nsig_sc is empty")
-	}
-	if p.nsig_check == "" {
-		t.Errorf("p.nsig_check is empty")
+	if p.sig_timestamp < 0 {
+		t.Errorf("p.sig_timestamp is negative")
 	}
 	if p.visitorData == "" {
-		t.Errorf("p.visitorData is empty")
+		t.Fatalf("p.visitorData is empty")
 	}
 
 	visitorData, err := url.QueryUnescape(p.visitorData)
@@ -42,12 +30,21 @@ func TestPlayer(t *testing.T) {
 		t.Errorf("failed to decode visitorData: %v", err)
 	}
 
-	_, err = parser.ParseFile(nil, "", p.nsig_sc, 0)
-	if err != nil {
-		t.Errorf("failed to parse p.nsig_sc: %v", err)
+	if p.nsig_sc != "" {
+		if p.nsig_name == "" {
+			t.Errorf("p.nsig_name is empty while p.nsig_sc is set")
+		}
+
+		_, err = parser.ParseFile(nil, "", p.nsig_sc, 0)
+		if err != nil {
+			t.Errorf("failed to parse p.nsig_sc: %v", err)
+		}
 	}
-	_, err = parser.ParseFile(nil, "", p.sig_sc, 0)
-	if err != nil {
-		t.Errorf("failed to parse p.sig_sc: %v", err)
+
+	if p.sig_sc != "" {
+		_, err = parser.ParseFile(nil, "", p.sig_sc, 0)
+		if err != nil {
+			t.Errorf("failed to parse p.sig_sc: %v", err)
+		}
 	}
 }
